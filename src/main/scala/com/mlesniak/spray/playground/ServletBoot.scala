a5b533e03c726971646e58c4a35354355a49078a
Michael Lesniak <mlesniak@micromata.de>
2016-02-22 00:30:17 +0100

    Format code

--- src/main/scala/com/mlesniak/spray/playground/ServletBoot.scala
+++ src/main/scala/com/mlesniak/spray/playground/ServletBoot.scala
@@ -5,8 +5,6 @@ import spray.servlet.WebBoot
 
 class ServletBoot extends WebBoot {
   implicit val system: ActorSystem = ActorSystem("SprayApiApp")
-
   val service = system.actorOf(Props(new RequestActor))
-
   val serviceActor: ActorRef = service
 }
