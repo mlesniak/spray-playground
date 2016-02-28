59034e7ff8ebd38289988aa0880daa6cc1d8e514
Michael Lesniak <mlesniak@micromata.de>
2016-02-28 17:24:40 +0100

    Scribble for simple TODO application

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -1,6 +1,9 @@
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('MainCtrl', function ($scope, $http) {
+    // List of values
+    $scope.todos = [];
+
     $http({
         url: '/api/time'
     }).then(function (data) {
@@ -9,6 +12,11 @@ mainApp.controller('MainCtrl', function ($scope, $http) {
 
     $scope.submit = function () {
         console.log("Clicked: " + $scope.text);
+        $scope.todos.push($scope.text);
         $scope.text = '';
+    }
+
+    $scope.remove = function (index) {
+        $scope.todos.splice(index, 1);
     }
 });
