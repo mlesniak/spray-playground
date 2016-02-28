551157b4202b6f7f71c4ad17c370931c2b46a333
Michael Lesniak <mlesniak@micromata.de>
2016-02-28 17:15:41 +0100

    AngularJS input handling

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -5,5 +5,10 @@ mainApp.controller('MainCtrl', function ($scope, $http) {
         url: '/api/time'
     }).then(function (data) {
         $scope.value = data["data"]
-    })
+    });
+
+    $scope.submit = function () {
+        console.log("Clicked: " + $scope.text);
+        $scope.text = '';
+    }
 });
