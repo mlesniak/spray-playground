1fd159b8155c4d224dda05ad3867503316989627
Michael Lesniak <mlesniak@micromata.de>
2016-02-28 23:40:18 +0100

    Focus input area

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -1,5 +1,25 @@
 var mainApp = angular.module('mainApp', []);
 
+// http://www.angulartutorial.net/2014/04/angular-js-auto-focus-for-input-box.html
+mainApp.directive('focus',
+    function ($timeout) {
+        return {
+            scope: {
+                trigger: '@focus'
+            },
+            link: function (scope, element) {
+                scope.$watch('trigger', function (value) {
+                    if (value === "true") {
+                        $timeout(function () {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    });
+
+
 mainApp.controller('MainCtrl', function ($scope, $http) {
     // List of values
     $scope.todos = ["foo", "bar"];
@@ -13,14 +33,17 @@ mainApp.controller('MainCtrl', function ($scope, $http) {
     $scope.submit = function () {
         $scope.todos.push($scope.text);
         $scope.text = '';
-    }
+    };
 
     $scope.remove = function (index) {
         $scope.todos.splice(index, 1);
-    }
+    };
 
     $scope.edit = function (index, value) {
         console.log("Edit: " + index);
         $scope.todos[index] = value;
+        $scope.entry = true;
     };
+
+    $scope.entry = true;
 });
