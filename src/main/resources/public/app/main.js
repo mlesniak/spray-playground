c9c441fc93abda060a36fe344a1341d77d21c663
Michael Lesniak <mlesniak@micromata.de>
2016-02-28 14:24:36 +0100

    Call api endpoint from angular

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -1,5 +1,9 @@
 var mainApp = angular.module('mainApp', []);
 
-mainApp.controller('MainCtrl', function ($scope) {
-    $scope.value = 1 + 2 == 3;
+mainApp.controller('MainCtrl', function ($scope, $http) {
+    $http({
+        url: '/api/time'
+    }).then(function (data) {
+        $scope.value = data["data"]
+    })
 });
